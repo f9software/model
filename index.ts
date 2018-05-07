03d8b97cb5b112b8db10795c54cd0217d5f01a37
Iulian Ilea <iulian@f9software.com>
2018-05-07 16:51:30 +0300

    Export more entities in index.ts.

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
 export {Model} from "./src/Model";
+export {Data} from './src/Data';
+export {Field} from './src/Field';
+export {Manager} from './src/Manager';
+export {Record} from './src/Record';
+export {ReducedRecord} from './src/Reducer';
