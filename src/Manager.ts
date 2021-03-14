b26a97e74b12161cdfdc07aed5d1f2d60d398e8a
Iulian Ilea <iulian.ilea@gmail.com>
2021-03-14 13:40:32 +0200

    Documentation.

--- src/Manager.ts
+++ src/Manager.ts
@@ -1,18 +1,22 @@
 import {Model} from './Model';
 import {Collection} from '@f9software/collection';
 
+/**
+ * Manager is aware of all the models registered with it. It's main purpose is to keep track of the models and be able to return
+ * the model when asked for it through getModel method.
+ */
 export class Manager {
-    private static models: Collection<Model<any>> = new Collection(model => model.getId());
+    private static models: Collection<Model<unknown>> = new Collection(model => model.getId());
 
-    public static registerModel(model: Model<any>) {
+    public static registerModel(model: Model<unknown>) {
         this.models.add(model);
     }
 
-    public static unregisterModel(model: Model<any>) {
+    public static unregisterModel(model: Model<unknown>) {
         this.models.remove(model);
     }
 
-    public static getModel<T>(id: string): Model<T> | undefined {
+    public static getModel<T extends unknown = unknown>(id: string): Model<T> | undefined {
         return this.models.get(id);
     }
 }
